71cf32f638714c7d39ec77a7b5636cdc4d93b1be
milka47 <73564447+milka47@users.noreply.github.com>
2021-02-18 19:40:39 -0800

    Add files via upload

---
+++ jokes.py
@@ -0,0 +1,20 @@
+print('What do you get when you cross a snowman with a vampire?')
+input()
+print('Frostbite! \n')
+
+print('What do dentists call an astronaut\'s cavity?')
+# an \ means you can print a ' and not have it mean that the string finished
+# if you actuall want to display the \, type it as \\, \t =tab ; \' single quote
+input()
+print('A black hole! \n')
+
+print('Knock knock.')
+input()
+print("Who's there?")
+input()
+print('Interrupting cow.')
+input()
+print('Interrupting cow wh', end='')
+# end='' is called keyword parameter and it's used to add strings at the end
+#the end= prints -MOO in the same line as the prevous line 
+print("-MOO!")
